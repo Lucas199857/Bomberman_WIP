4f220c4ab4196a5c1b0176ef8f92935009fb181e
Lucas199857 <43035377+Lucas199857@users.noreply.github.com>
2018-09-10 14:40:54 +0200

    Add files via upload

--- Code/Bomberman_JS.js
+++ Code/Bomberman_JS.js
@@ -366,6 +366,7 @@ function GameOver(){
 
 function start(){
     document.getElementById("myCanvas").style.visibility = "visible";
+    document.getElementById("Impressum").style.visibility="hidden";
     document.getElementById("Start").style.visibility="hidden";
     document.getElementById("GameOver").style.visibility = "hidden";
     document.getElementById("Controls").style.visibility="hidden";
@@ -394,6 +395,7 @@ function menue(){
     document.getElementById("Controls").style.visibility="visible";
     document.getElementById("Kasten").style.visibility="visible";
     document.getElementById("Bomberman").style.visibility="visible";
+    document.getElementById("Impressum").style.visibility="visible";
     document.getElementById("menue").style.visibility="hidden";
     document.getElementById("GameOver").style.visibility = "hidden";
     document.getElementById("Player1").style.visibility="hidden";
@@ -415,8 +417,28 @@ function menue(){
 }
 function controls(){
     document.getElementById("Start").style.visibility="hidden";
+    document.getElementById("Impressum").style.visibility="hidden";
     document.getElementById("Controls").style.visibility="hidden";
     document.getElementById("Player1").style.visibility="visible";
     document.getElementById("Player2").style.visibility="visible";
     document.getElementById("menue").style.visibility="visible";
+}
+function impressum(){
+    document.getElementById("Start").style.visibility="hidden";
+    document.getElementById("Impressum").style.visibility="hidden";
+    document.getElementById("Bomberman").style.visibility="hidden";
+    document.getElementById("Bomb2").style.visibility="hidden";
+    document.getElementById("Controls").style.visibility="hidden";
+    document.getElementById("Kasten").style.visibility="hidden";
+    document.getElementById("impressum").style.visibility="visible";
+    document.getElementById("menueim").style.visibility="visible";
+}
+
+function menue2(){
+    document.getElementById("Start").style.visibility="visible";
+    document.getElementById("Controls").style.visibility="visible";
+    document.getElementById("Kasten").style.visibility="visible";
+    document.getElementById("Bomberman").style.visibility="visible";
+    document.getElementById("Impressum").style.visibility="visible";
+    document.getElementById("menue").style.visibility="hidden";
 }
